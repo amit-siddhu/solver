b2292207b6c3110761274e227b17e7bddd35ee30
Amit Siddhu <amit.siddhu7@gmail.com>
2013-12-06 09:47:57 +0530

    Initial Commit

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,109 @@
+class SearchesController < ApplicationController
+  # GET /searches
+  # GET /searches.json
+  def index
+   @dict = Array.new
+    @Flag = false
+    @match = ''
+  if request.post? && !params[:input].nil?
+
+    f = File.new("public/some.txt","r")
+      f.each_line do |l|
+      @dict.push(l.squish)
+    end
+
+    @dict.each do |dt|
+
+      if dt.length == (params[:input].squish).length
+        
+        if dt.chars.sort.join == (params[:input].squish).chars.sort.join
+
+          @flag = true
+          @match = dt
+          redirect_to root_path, notice: 'Keyword Found in dictionary:  '+dt 
+          break
+
+        end
+
+      end
+
+
+
+    end
+
+
+
+  end
+  end
+
+  # GET /searches/1
+  # GET /searches/1.json
+  def show
+    @search = Search.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @search }
+    end
+  end
+
+  # GET /searches/new
+  # GET /searches/new.json
+  def new
+    @search = Search.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @search }
+    end
+  end
+
+  # GET /searches/1/edit
+  def edit
+    @search = Search.find(params[:id])
+  end
+
+  # POST /searches
+  # POST /searches.json
+  def create
+    @search = Search.new(params[:search])
+
+    respond_to do |format|
+      if @search.save
+        format.html { redirect_to @search, notice: 'Search was successfully created.' }
+        format.json { render json: @search, status: :created, location: @search }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @search.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /searches/1
+  # PUT /searches/1.json
+  def update
+    @search = Search.find(params[:id])
+
+    respond_to do |format|
+      if @search.update_attributes(params[:search])
+        format.html { redirect_to @search, notice: 'Search was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @search.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /searches/1
+  # DELETE /searches/1.json
+  def destroy
+    @search = Search.find(params[:id])
+    @search.destroy
+
+    respond_to do |format|
+      format.html { redirect_to searches_url }
+      format.json { head :no_content }
+    end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Solver::Application.config.secret_token = '2d0af5d8a21a5190e98bdcdc557b679926b5b3b785051b76aaba6688ea56439ad7db5f126ff4c2a755b5d46b053b6a22d950b0f79f674a2d64b13068b219aa96'
